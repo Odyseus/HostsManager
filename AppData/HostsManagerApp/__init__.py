3cb53da709fb03e26cfc6f7bcffbfa5c1aabf622
Odyseus <venusyadonis@gmail.com>
2018-09-16 04:39:02 -0300

    Added README and updated __init__.py module.

--- AppData/HostsManagerApp/__init__.py
+++ AppData/HostsManagerApp/__init__.py
@@ -5,3 +5,7 @@ __status__ = "(Stable)"
 __appname__ = "Hosts Manager"
 __appdescription__ = "A CLI utility to manage hosts files on GNU/Linux."
 __version__ = "2018-09-15 01:19:13.336"
+
+
+if __name__ == "__main__":
+    pass
---
+++ README.md
@@ -0,0 +1,4 @@
+
+## Hosts Manager ([documentation](https://pythoncliapplications.gitlab.io/CLIApplicationsManager/includes/HostsManager/index.html))
+
+A CLI utility to manage hosts files on GNU/Linux.
