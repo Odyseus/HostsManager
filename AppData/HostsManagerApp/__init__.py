ddbe43e8a904ac88acfe934090aa6d5b6760e837
Odyseus <venusyadonis@gmail.com>
2018-12-15 07:32:50 -0300

    __init__.py
    - Homogenization of the __appdescription__ variable.

--- AppData/HostsManagerApp/__init__.py
+++ AppData/HostsManagerApp/__init__.py
@@ -3,7 +3,7 @@
 
 __status__ = "Stable"
 __appname__ = "Hosts Manager"
-__appdescription__ = "A CLI utility to manage hosts files on GNU/Linux."
+__appdescription__ = "CLI utility to manage hosts files on GNU/Linux."
 __version__ = "2018-09-15 01:19:13.336"
 
 
