b1a8539779f0a4081a6778818b681e1f01d70307
Odyseus <venusyadonis@gmail.com>
2020-02-14 03:49:28 -0300

    Bump version

--- AppData/HostsManagerApp/__init__.py
+++ AppData/HostsManagerApp/__init__.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
 __status__ = "Stable"
 __appname__ = "Hosts Manager"
 __appdescription__ = "CLI utility to manage hosts files on GNU/Linux."
-__version__ = "2018-09-15 01:19:13.336"
-
+__version__ = "2020-02-14 03:47:21.875"
 
 if __name__ == "__main__":
     pass
